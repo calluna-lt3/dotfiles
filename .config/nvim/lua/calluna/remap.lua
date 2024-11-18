6486cd6311968a5ca57adc5e587d632208341d92
Calluna <106458302+CallunaLessThan3@user.noreply.github.com>
2024-11-18 14:25:46 -0800

    reformat

--- .config/nvim/lua/calluna/remap.lua
+++ .config/nvim/lua/calluna/remap.lua
@@ -1,8 +1,30 @@
 -- Commands
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Copy/paste system clipboard
+vim.keymap.set({"n", "v"}, "<leader>sy", [["+y]])
+vim.keymap.set({"n", "v"}, "<leader>sp", [["+p]])
+
+-- Put from register, dont yank deleted content
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Center screen on various movements
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "<C-i>", "<C-i>zz")
+vim.keymap.set("n", "<C-o>", "<C-o>zz")
+vim.keymap.set({"n", "v"}, "gk", "ggzz")
+vim.keymap.set({"n", "v"}, "gj", "Gzz")
+
+-- Vim command window --> insert mode
+vim.keymap.set("n", "<C-f>", "q:")
+
+-- TODO: something using substitute :'<,'>s\(.*\)/<something>/g
 
 -- Compile & Run
+-- TODO: this is kinda trash
 vim.keymap.set("n", "<leader>pc",
     function()
         vim.cmd(":w")
@@ -16,23 +38,3 @@ vim.keymap.set("n", "<leader>pr",
     end
 )
 
--- Copy/paste system clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>p", "\"+p")
-vim.keymap.set("v", "<leader>p", "\"+p")
-
--- Center screen on various movements
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
-vim.keymap.set("n", "<C-i>", "<C-i>zz")
-vim.keymap.set("n", "<C-o>", "<C-o>zz")
-vim.keymap.set("n", "gk", "ggzz")
-vim.keymap.set("n", "gj", "Gzz")
-vim.keymap.set("v", "gk", "ggzz")
-vim.keymap.set("v", "gj", "Gzz")
-
--- Vim command window --> insert mode
-vim.keymap.set("n", "<C-f>", "q:")
