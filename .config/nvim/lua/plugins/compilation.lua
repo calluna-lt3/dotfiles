125ac3694cd05dab09275e1b8711c53d957c5862
calluna <106458302+callunalessthan3@users.noreply.github.com>
2025-03-13 01:27:39 -0700

    [nvim] compilation mode config

--- .config/nvim/lua/plugins/compilation.lua
+++ .config/nvim/lua/plugins/compilation.lua
@@ -3,7 +3,11 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     key = { '<leader>c', '<leader>r', '<leader>n' },
     config = function ()
-        vim.g.compile_mode = {}
+        vim.g.compile_mode = {
+            default_command = "",
+            recompile_no_fail = true,
+            auto_jump_to_first_error = true,
+        }
         vim.keymap.set("n", "<leader>c", ":below Compile<CR>")
         vim.keymap.set("n", "<leader>r", ":w<CR>:below Recompile<CR>")
         vim.keymap.set("n", "<leader>n", ":NextError<CR>")
