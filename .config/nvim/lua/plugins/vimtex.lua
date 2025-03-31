ee6cde340bb3d64a6a6f062441c1f3beaa8dedf1
calluna <106458302+callunalessthan3@users.noreply.github.com>
2025-03-30 18:11:06 -0700

    [nvim] add vimtex

---
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -0,0 +1,13 @@
+return {
+    'lervag/vimtex',
+
+    init = function()
+        vim.cmd([[
+            let g:vimtex_view_method = "zathura"
+            let g:vimtex_compiler_method = 'latexmk'
+            let g:vimtex_quickfix_mode=2
+            let g:tex_conceal='abdmg'
+            set conceallevel=2
+        ]])
+    end,
+}
